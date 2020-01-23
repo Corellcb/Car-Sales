4cea65cc48d8f96fe11dce6f5b85065a2772e20b
CorellCB <corellcb@gmail.com>
2020-01-23 15:04:06 -0500

    refactored state for redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './App';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import { Carreducer } from './components/reducers/Carreducer';
+
+const store = createStore(Carreducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store} >
+    <App />
+</Provider>, 
+rootElement
+);
